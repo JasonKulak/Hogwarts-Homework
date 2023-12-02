bf4c4b6d3566e648d17ffcfd0d3858400b67c725
Jason Kulak <JasonKulak@yahoo.com>
2023-12-01 21:18:43 -0600

    Commit 1 - I added my first element and it was fun

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 // if (typeof jQuery == 'undefined'){
 //     console.log('oops! I still have to link my jQuery properly!');
 //   } else {console.log('I did it! I linked jQuery and this js file!')};
+
+$(() => {
+	// put jQuery in here
+    $container = $("#container")
+    console.log($container)
+
+    $h1 = $("<h1>")
+    //console.log($h1)
+    $h1.text("Hogwarts")
+    console.log($h1)
+    $container.append($h1)
+});
