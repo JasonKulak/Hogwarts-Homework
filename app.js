19c59bb132612ea2d19c0fc4c7f58d062969b250
Jason Kulak <JasonKulak@yahoo.com>
2023-12-03 14:12:38 -0600

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- app.js
+++ app.js
@@ -5,11 +5,29 @@
 $(() => {
 	// put jQuery in here
     $container = $("#container")
-    console.log($container)
+    //console.log($container)
 
     $h1 = $("<h1>")
     //console.log($h1)
     $h1.text("Hogwarts")
-    console.log($h1)
+    //console.log($h1)
     $container.append($h1)
+
+    $h2 = $("<h2>")
+    $h2.text("Jason")
+    $container.append($h2)
+
+    $h3 = $("<h3>")
+    $h3.text("Gryffindor")
+    $container.append($h3)
+
+    $h4 = $("<h4>")
+    $h4.text("Lord Stark")
+    const $input = $("input")
+    const val = $input.val();
+    $h4.addClass("cat").text(val);
+    // const wand = $input.wand();
+    // $h4.text("Hawthorn Wand with Dragon Heartstring Core")
+    
+    $container.append($h4)
 });
